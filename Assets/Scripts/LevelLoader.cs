6afcf10b762d6d9393e6e1ad777df40e25165169
dimitriszov <dimzovo@gmail.com>
2023-05-12 20:46:55 +0300

    Added loading screen
    Loading screen added with a progress bar for changing scenes. It is done using the SettingsObj

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class LevelLoader : MonoBehaviour
 {
+    [SerializeField]
+    public GameObject loadingPanel;
+    [SerializeField]
+    public GameObject settingsPanel;
+    [SerializeField] 
+    public  RectTransform fxHolder;
+    [SerializeField] 
+    public UnityEngine.UI.Image circle;
+    [SerializeField] 
+    public Text progressText;
     public void loadLevel(string name)
     {
         StartCoroutine(LoadAsychronously(name));
@@ -11,11 +23,23 @@ public class LevelLoader : MonoBehaviour
 
     IEnumerator LoadAsychronously(string name)
     {
+        settingsPanel.SetActive(false);
+        loadingPanel.SetActive(true);
         AsyncOperation operation = SceneManager.LoadSceneAsync(name);
         while (!operation.isDone)
         {
-            Debug.Log(operation.progress);
+            float progress = Mathf.Clamp01(operation.progress/ .9f);
+            progressText.text = Mathf.Floor(progress * 100).ToString();
+            fxHolder.rotation = Quaternion.Euler(new Vector3(0f, 0f, -progress*360));
+            Debug.Log(progress);
             yield return null;
         }
+        loadingPanel.SetActive(false);
+    }
+
+    public void closeApp()
+    {
+        Debug.Log("Closing App");
+        Application.Quit();
     }
 }
