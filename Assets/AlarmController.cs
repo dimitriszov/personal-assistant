6ef01dc5f427d3c61f8193462a8c585f424f07f9
evanthiakk <evkokkinidou@gmail.com>
2023-05-13 21:30:22 +0200

    clock

---
+++ Assets/AlarmController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using TMPro;
+
+
+public class AlarmController : MonoBehaviour
+{
+    [SerializeField] private Text timeText;
+    [SerializeField] private TMP_InputField hoursInput, minutesInput, secondsInput;
+    [SerializeField] private TMP_Dropdown dropdown;
+    [SerializeField] private GameObject alarmPanel;
+    private bool isAlarmSet = false;
+    private DateTime alarmTime = DateTime.Today;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int hours = DateTime.Now.Hour;
+        int minutes = DateTime.Now.Minute;
+        int seconds = DateTime.Now.Second;
+
+
+        bool isAM = hours < 12;
+
+        timeText.text = $"{hours % 12:D2}: {minutes:D2}: {seconds:D2} {(isAM ? "AM": "PM")}";
+
+        if(isAlarmSet && DateTime.Now > alarmTime)
+        {
+            //Debug.Log("ALARM");
+            alarmPanel.SetActive(true);
+        }
+
+    }
+
+
+    public void SetAlarm()
+    {
+
+        alarmTime = DateTime.Today;
+
+        int hours;
+        if (dropdown.value == 0)
+        {
+            hours = int.Parse(hoursInput.text);
+        }
+        else;
+        {
+            hours = int.Parse(hoursInput.text) + 12;
+        }
+        TimeSpan ts = TimeSpan.Parse($"{hours}:{minutesInput.text}: {secondsInput.text}");
+        alarmTime += ts;
+
+        if (DateTime.Now >= alarmTime)
+        {
+            alarmTime = alarmTime.AddDays(1);
+
+        }
+       // Debug.Log(alarmTime);
+
+        isAlarmSet = true;
+
+    }
+}
---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using TMPro;
+
+public class Clock : MonoBehaviour
+{
+
+    public Text clockText;
+  
+
+    private void Awake()
+    {
+        clockText = GetComponent<Text>();
+
+    }
+   
+
+    // Update is called once per frame
+    private void Update()
+    {
+        DateTime time = DateTime.Now;
+        string hour =LeadingZero(time.Hour);
+
+        string minute = LeadingZero(time.Minute);
+
+        string second = LeadingZero(time.Second);
+
+        clockText.text = hour + ":" + minute + ":" + second;
+
+    }
+
+    string LeadingZero( int n)
+    {
+        return n.ToString().PadLeft(2, '0');
+    }
+}
