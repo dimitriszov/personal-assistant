e95bc76b62b0f113cbb53bf31c22dc56cf31361c
dimitriszov <dimzovo@gmail.com>
2023-05-23 15:55:12 +0300

    Search scene completed

---
+++ Assets/Scripts/DropdownHandler.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class DropdownHandler : MonoBehaviour
+{
+    public LevelLoader levelLoader;
+
+    public void HandleInputData(int val)
+    {
+        switch (val)
+        {
+            case 0:
+                levelLoader.loadLevel("EmailScene");
+                break;
+            case 1:
+                Application.OpenURL("https://mail.google.com/");
+                break;
+            case 2:
+                levelLoader.loadLevel("SearchScene");
+                break;
+            case 3:
+                levelLoader.loadLevel("NavigationScene");
+                break;
+            case 4:
+                levelLoader.loadLevel("WeatherScene");
+                break;
+            case 5:
+                levelLoader.loadLevel("Diary");
+                break;
+            case 6:
+                levelLoader.loadLevel("Clock");
+                break;
+            case 7:
+                levelLoader.loadLevel("Contacts");
+                break;
+            /*case 8:
+                levelLoader.loadLevel("SearchScene");
+                break;
+            case 9:
+                levelLoader.loadLevel("SearchScene");
+                break;
+            case 10:
+                levelLoader.loadLevel("SearchScene");
+                break;
+            case 11:
+                levelLoader.loadLevel("SearchScene");
+                break;
+            case 12:
+                levelLoader.loadLevel("SearchScene");
+                break;
+            case 13:
+                levelLoader.loadLevel("SearchScene");
+                break;
+            case 14:
+                levelLoader.loadLevel("SearchScene");
+                break;
+                */
+        }
+    }
+}
---
+++ Assets/Scripts/SearchManager.cs
@@ -0,0 +1,98 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using TMPro;
+using System.Net.Mail;
+
+public class SearchManager : MonoBehaviour
+{
+    public TMP_InputField input;
+    [SerializeField] public Transform container;
+    [SerializeField] public GameObject prefab;
+    private string APIkey = "AIzaSyCpvJ4yyPfI2Qz2s9crxSZyh1LRuqhrj1I";
+    private string searchEngineId = "4715c1f418ff84742";
+
+    // Models for deserialization
+    [Serializable]
+    public class SearchResponse
+    {
+        public List<Result> Items { get; set; }
+    }
+
+    [Serializable]
+    public class Result
+    {
+        public string Link { get; set; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void search()
+    {
+        StartCoroutine(GetResults(input.text));
+    }
+
+    IEnumerator GetResults(string query)
+    {
+        // Build the API request URL
+        string apiUrl = $"https://www.googleapis.com/customsearch/v1?key={APIkey}&cx={searchEngineId}&q={query}";
+        Debug.Log(apiUrl);
+        using (UnityWebRequest request = UnityWebRequest.Get(apiUrl))
+        {
+            request.timeout = 20;
+            yield return request.SendWebRequest();
+
+            // did the request succeed?
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                // Process the JSON response
+                string responseContent = request.downloadHandler.text;
+                // Deserialize the JSON response
+                SearchResponse searchResponse = JsonConvert.DeserializeObject<SearchResponse>(responseContent);
+
+                // Extract the results
+                List<Result> results = searchResponse.Items;
+
+                // Limit the results to the first 20 sites
+                List<Result> first20Results = results.GetRange(0, Math.Min(20, results.Count));
+
+                // Print the URLs of the first 20 sites
+                foreach (Result result in first20Results)
+                {
+                    Debug.Log(result.Link);
+                    addResult(result.Link);
+                }
+
+                // For simplicity, we'll just print the response content
+                Debug.Log(responseContent);
+            }
+            else
+            {
+                Debug.LogError($"Request failed with error: {request.error}");
+            }
+        }
+        yield return null;
+    }
+
+    public void addResult(string link)
+    {
+        GameObject result = Instantiate(prefab, container);
+        if (result.TryGetComponent<SearchResult>(out SearchResult item))
+        {
+            item.changeText(link);
+        }
+    }
+}
